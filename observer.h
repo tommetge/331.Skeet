356af61eb84e2c182456358fe1dd10b41a4972bc
tom metge <tom@metge.us>
2022-02-08 16:36:24 -0800

    Add templates matching design

---
+++ observer.h
@@ -0,0 +1,18 @@
+/***********************************************************************
+ * Header File:
+ *    Observer : Defines an observer class
+ * Author:
+ *    Team 1
+ * Summary:
+ *    Defines a observer (pattern) class
+ ************************************************************************/
+
+#pragma once
+
+/**********************
+ * Observer: Defines an observer (pattern) class
+ **********************/
+class Observer
+{
+
+};

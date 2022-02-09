a52f8d841b7706d5409395a166972e2a1eccd83f
tom metge <tom@metge.us>
2022-02-08 16:35:07 -0800

    Add templates matching design

---
+++ killable.h
@@ -0,0 +1,18 @@
+/***********************************************************************
+ * Header File:
+ *    Killable : Defines a killable class
+ * Author:
+ *    Team 1
+ * Summary:
+ *    Defines a killable class
+ ************************************************************************/
+
+#pragma once
+
+/**********************
+ * Killable: Defines a killable class
+ **********************/
+class Killable
+{
+
+};
---
+++ mediator.h
@@ -0,0 +1,18 @@
+/***********************************************************************
+ * Header File:
+ *    Mediator : Abstract definition of a Mediator class
+ * Author:
+ *    Team 1
+ * Summary:
+ *    Defines a mediator (pattern) class
+ ************************************************************************/
+
+#pragma once
+
+/**********************
+ * Mediator: Abstract definition of a Mediator pattern class
+ **********************/
+class Mediator
+{
+
+};

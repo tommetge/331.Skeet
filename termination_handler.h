26eedd163eebfb6f093d36e9c89cd8f968ba69c5
tom metge <tom@metge.us>
2022-02-08 16:31:50 -0800

    Add templates matching design

---
+++ termination_handler.h
@@ -0,0 +1,18 @@
+/***********************************************************************
+ * Header File:
+ *    TerminationHandler : Chain of Command-inspired termination handler
+ * Author:
+ *    Team 1
+ * Summary:
+ *    Defines a terminable class
+ ************************************************************************/
+
+#pragma once
+
+/**********************
+ * TerminationHandler: Chain of Command-inspired termination handler
+ **********************/
+class TerminationHandler
+{
+
+};
